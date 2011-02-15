5a1ec6516b664900f25dd052b634abc427075479
Kim Burgestrand <kim@burgestrand.se>
2011-02-15 15:43:26 +0100

    Fetch templates explicitly from GitHub

--- slim/slim_builder.rb
+++ slim/slim_builder.rb
@@ -1,18 +1,24 @@
-class AppBuilder < Rails::AppBuilder
-  def initialize(*args)
-    super
-    source_paths.unshift File.expand_path('../template', __FILE__)
-  end
+require 'tempfile'
 
+class AppBuilder < Rails::AppBuilder
   def config
     super
     inside('config') do
       remove_dir 'initializers'
       remove_dir 'locales'
-      
-      template './application.rb'
-      directory './environments'
     end
+      
+    get_from_github 'config/application.rb', :force => true
+    get_from_github 'config/routes.rb', :force => true
+    [:production, :development, :test].each do |file|
+      get_from_github "config/environments/#{file}.rb.tt", :force => true
+    end
+  end
+  
+  def app
+    super
+    get_from_github 'app/controllers/application_controller.rb.tt', :force => true
+    get_from_github 'app/views/layouts/application.html.erb.tt', :force => true
   end
   
   # noop
@@ -50,4 +56,20 @@ class AppBuilder < Rails::AppBuilder
       copy_file 'favicon.ico', 'apple-touch-icon.png'
     end
   end
+  
+  protected
+    def github
+      @__github__ ||= Hash.new { |h, (k, v)| h[k] = "https://github.com/Burgestrand/rails-templates/raw/master/slim/template/#{k}" }
+    end
+    
+    def get_from_github(source, config = {})
+      get_template github[source], source.gsub(/\.tt\Z/, ''), config
+    end
+  
+    def get_template(source, destination = nil, config = {})
+      destination ||= ::File.basename(source)
+      Tempfile.open('') do |f|
+        template get(source, f.path, :force => true), destination, config
+      end
+    end
 end
